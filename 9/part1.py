fdd8a3959345ecde3b300a27a3dd22156c77b573
Qb <qb@qbit.moe>
2023-12-11 18:45:49 +0100

    day 9 part 1, first working solution

---
+++ 9/part1.py
@@ -0,0 +1,19 @@
+import sys
+
+lines = open(sys.argv[1] if len(sys.argv)>1 else "./input.txt", "r").readlines()
+
+def derive(nos):
+    return [nos[i+1]-nos[i] for i in range(len(nos)-1)]
+
+def extrapolate(nos):
+    if all(no==0 for no in nos):
+        return 0
+    else:
+        return nos[-1] + extrapolate(derive(nos))
+
+result = 0
+for line in lines:
+    nos = [int(no) for no in line.strip().split()]
+    result += extrapolate(nos)
+
+print(result)
